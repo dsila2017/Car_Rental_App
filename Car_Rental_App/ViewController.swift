568443b0d0aeefadb8f5750c2bfd9efa0d4a4372
dsila2017 <dsila2017@gmail.com>
2025-02-18 22:33:52 +0400

    Initial Commit

---
+++ Car_Rental_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Car_Rental_App
+//
+//  Created by David on 2/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
